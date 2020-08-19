884397298dd89e1dc3372f7511d14b391421cbca
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-08-19 19:34:32 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ bot.on("ready", function (){
     console.log(`${bot.user.username} está online`);
 });
 
-bot.login(process.enc.token);
+bot.login("NzQ0MDE0NTg2MDU4ODk5NDk3.XzdDlA.cWrCFe0ut8THjPj2oLY7nYxcPkg");
