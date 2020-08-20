451e24fe213ea08b04787ae70866f90da98211d2
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-08-20 17:34:18 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,8 +26,8 @@ client.on("ready", () => {
   var tabela = [
     {name: '[LEAKS] G-Force Community - https://discord.gg/dyPAPG', type:'PLAYING'},
     {name: 'Criado por uGabriel#2193', type: 'LISTENING'},
-    {name: 'Veja meus comandos: /ajuda', type: 'WATCHING'},
-    {name: 'V1 -> V2'}
+    {name: 'Veja meus comandos: ${prefix}ajuda', type: 'WATCHING'},
+    {name: 'Versão: 2.0.2'}
   ];
 
   function setStatus() {
