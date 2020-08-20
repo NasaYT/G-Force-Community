85f4ec4a0ab698ff2655be751bdc7674cd339bcd
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-08-19 23:39:07 -0300

    Add files via upload

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -6,15 +6,15 @@ exports.run = (client, message, args) => {
     let embed = new Discord.MessageEmbed()
     .setColor([28, 255, 103])
     .setAuthor("Informação do Bot") // message.author.avatarURL - Pegar do usuário que digitou o cmd
-    .setDescription(":seta: **Meu criador:** uGabriel#2193")
-    .addField("Servidores", ":desktop: " + client.guilds.cache.size, true)
-    .addField("Usuários", ":joystick: " + client.users.cache.size, true)
-    .addField("Canais", ":page_facing_up: " + client.channels.cache.size, true)
-    .addField("Versão do bot", "2.0", true)
-    .addField("País", ":flag_br: Brasil", true)
-    .addField("Livraria", ":books: discord.js", true)
+    .setDescription("\<:seta:745286344850276363> **Meu criador:** \n uGabriel#2193")
+    .addField(":desktop: Servidores", + client.guilds.cache.size, true)
+    .addField(":joystick: Usuários", + client.users.cache.size, true)
+    .addField(":page_facing_up: Canais", + client.channels.cache.size, true)
+    .addField(":wrench: Versão do bot", "2.0.2", true)
+    .addField(":flag_br: País", "Brasil", true)
+    .addField(":books: Tipo", "discord.js", true)
     //.addField("Criado em", ":inbox_tray: " + client.user.createdAt, true)
-    .addField('Criado em', ":inbox_tray: " + formatDate('DD/MM/YYYY, às HH:mm:ss', date))
+    .addField(':inbox_tray: Criado em', + formatDate('DD/MM/YYYY, às HH:mm:ss', date))
     .setFooter(client.user.username, client.user.displayAvatarURL()) // para pegar o bot name: client.user.username
     // sendo message.author | client.user / pega o id do author e o id do bot
     .setTimestamp();
