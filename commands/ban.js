8ea939488403f5ab64a08d0b4b15d216f6e8d25a
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-08-20 00:11:19 -0300

    Add files via upload

--- commands/ban.js
+++ commands/ban.js
@@ -8,12 +8,12 @@ exports.run = async (client, message, args) => {
   .setTitle(`❓ INFORMAÇÃO DO COMANDO`)
   .setDescription(`\`ban\` - Dê Ban em um usuário`)
   .addField(`:hammer: **Uso**`, `\`${c.prefix}ban @nomedousuario motivo\``)
-  .addField(`:book: **Exemplo**`, `\`${c.prefix}ban @zKinG#6890 lindao\``)
+  .addField(`:book: **Exemplo**`, `\`${c.prefix}ban @uGabriel#2193 lindao\``)
   .addField(`:bookmark: **Permissão**`, `\`BAN_MEMBERS\``)
   .setColor('#8c0046')   
 
     //adicione o nome do cargo que vc quer que use esse comando!
-    if(!message.member.hasPermission("BAN_MEMBERS"))
+    if(!message.member.hasPermission("BAN_MEMBERS") && message.member.id !== '487965837404274689')
       return message.reply("Você não tem permissão para usar este comando!");
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     if(!member)
@@ -23,24 +23,24 @@ exports.run = async (client, message, args) => {
     let reason = args.slice(1).join(' ');
     if(!reason) reason = "\<:exclamacao:745286346641375283> | Razão não fornecida";
   
-  let embedi = new Discord.MessageEmbed()
+  let embed = new Discord.MessageEmbed()
 
         .setTitle(`:warning: G-Force Community`)
         .setFooter(`\<:seta1:745286347723374672> | Você foi banido da G-Force Community por ${message.author.username}`)
       
-    await member.send(embedi)
+    await member.send(embed)
     await member.ban(reason)
       .catch(error => message.reply(`\<:notificacao:745286344615395428> | Não consegui banir o usuário mencionado: ${error}`));
   
       let embed = new Discord.MessageEmbed()
-        .setTitle("🚫 Império Banimentos 🚫", message.author.avatarURL)
+        .setTitle("🚫 G-Force Community 🚫", message.author.avatarURL)
         .setThumbnail(message.author.avatarURL)
         .addField("📋Staff Tag", message.author, true)
         .addField("🚨Membro Tag", member, false)
         .addField("📝Motivo:", reason, true)
         .setColor("RANDOM").setTimestamp()
       
-      client.channels.cache.get('712451499564728380').send(embed)
+      client.channels.cache.get('745841974636052490').send(embed)
   }
 
   exports.help = {
