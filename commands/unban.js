23df99d8a15c7a90c80e296b77d7ec4df212e651
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-08-20 18:07:04 -0300

    Add files via upload

--- commands/unban.js
+++ commands/unban.js
@@ -13,7 +13,6 @@ exports.run = async (client, message, args) => {
     .setTitle("\<:notificacao:745286344615395428> G-Force Community", message.author.avatarURL)
     .setThumbnail(message.author.avatarURL)
     .addField("\<:notificacao:745286344615395428> Usuário Desbanido:", message.mentions.members.first(), false)
-    .addField("\<:diploma:745286344980561981> Motivo:", reason, true)
     .addField('\<:livro:745286345634742382> Data do desbanimento:', message.createdAt.toLocaleString())
 
     message.channel.send(embed)
