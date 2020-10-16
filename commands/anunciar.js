15ca5898830604064be308a9c466ba78d38146e0
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-10-16 11:33:43 -0300

    Update anunciar.js

--- commands/anunciar.js
+++ commands/anunciar.js
@@ -47,7 +47,8 @@ module.exports = {
             msg.edit(anuncioembed)
             msg.edit('@everyone')
             c1.remove(message.author.id)
-};
+    });
+}
 
 exports.help = {
   name: "aviso",
