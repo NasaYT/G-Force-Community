8df939f00d299e5db94871bd18b5f0d126e0a838
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-11-10 13:27:07 -0300

    Add files via upload

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -3,8 +3,7 @@ const request = require('snekfetch');
 exports.run = (client, message, args) => {
 
     const tnai = require('tnai'),
-    cli = new client(),
-    gif = cli.sfw.hug()
+    gif = client.sfw.hug()
     
     console.log(gif)
 }
