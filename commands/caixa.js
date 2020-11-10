4267e2b0318c0646d87c7704615dab3b771edf95
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-11-10 12:44:05 -0300

    Add files via upload

--- commands/caixa.js
+++ commands/caixa.js
@@ -1,10 +1,6 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => { 
-    let afirm = ["Discordo", "Concordo", "Tem razão"]
-    let rdm = Math.floor(Math.random() * afirm.length)
-    if (args[0] == null || args[0] == undefined) return
-    if (!args.join(" ").includes("?")) return message.channel.send(afirm[rdm])
 
     let resp = ["\<:NameTag:766086879837421599> Você abriu uma caixa e ganhou: **Par de Chifres*", "\<:NameTag:766086879837421599> Você abriu uma caixa e ganhou: **Uma namorada**", "\<:NameTag:766086879837421599> Você abriu uma caixa e ganhou: **Nada**", "\<:NameTag:766086879837421599> Você abriu uma caixa e ganhou: **1 dia de sorte**"]
     let random = Math.floor(Math.random() * resp.length)
@@ -13,5 +9,5 @@ exports.run = (client, message, args) => {
 
 exports.help = {
     name: 'abrircaixa',
-    aliases: []
+    aliases: [caixaabrir]
   }
