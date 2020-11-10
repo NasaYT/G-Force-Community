a68a7916f74cba3d42261f0075b89d253ced2e1d
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-11-10 13:14:38 -0300

    Add files via upload

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,13 +1,11 @@
 const akaneko = require('akaneko');
+const akaneko = require('discord.js');
  
 exports.run = (client, message, args) => { 
  
   console.log("SFW Neko: " + await akaneko.neko());
- 
   console.log("Lewd Neko:" + await akaneko.lewdNeko());
- 
   console.log("Lewd Bomb: " + await akaneko.lewdBomb(5));
- 
   console.log("BDSM: " + await akaneko.nsfw.bdsm());
   console.log("Maid: " + await akaneko.nsfw.maid());
   console.log("Hentai: " + await akaneko.nsfw.hentai());
