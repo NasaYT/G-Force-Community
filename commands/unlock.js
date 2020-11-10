ed8ca0cd347d61e941cd6e6a20eb64ae85f22509
uGabrielDemon <69701830+uGabrielDemon@users.noreply.github.com>
2020-11-10 12:26:49 -0300

    Update unlock.js

--- commands/unlock.js
+++ commands/unlock.js
@@ -4,12 +4,12 @@ const c = require('../config.json')
 
 exports.run = async (client, message, args) => {
 
-  let everyone = message.guild.roles.cache.find(x => x.name === "@everyone");
+  let everyone = message.guild.roles.cache.find(x => x.name === "Membro");
   message.channel.updateOverwrite(everyone, {
                 SEND_MESSAGES: true
               })
 
-              message.channel.send(` \<:coranteverde:745286344669921340> | O envio de mensagem desde canal foi habilitado.`);
+              message.channel.send(` \<:coranteverde:745286344669921340> | O envio de mensagem deste canal foi habilitado.`);
 }
 
 exports.help = {
